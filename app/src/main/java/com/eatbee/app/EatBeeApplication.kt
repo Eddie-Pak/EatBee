1c898d5859bd404ed90b370c3de0b138b9630270
Eddie-Pak <foalstn@naver.com>
2026-05-14 01:50:06 +0900

    feat: Hilt 설정

--- app/src/main/java/com/eatbee/app/EatBeeApplication.kt
+++ app/src/main/java/com/eatbee/app/EatBeeApplication.kt
@@ -1,7 +1,9 @@
 package com.eatbee.app
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class EatBeeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
--- presentation/src/main/java/com/eatbee/presentation/MainActivity.kt
+++ presentation/src/main/java/com/eatbee/presentation/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.eatbee.presentation.navigation.EatBeeNavHost
 import com.eatbee.presentation.ui.theme.EatBeeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
